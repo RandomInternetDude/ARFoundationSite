929f424e5ffe6eca5ec3a36258b5b9638989ac9c
サトシナカモト <jrivera@owners-MBP.lan>
2021-09-26 14:59:55 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="landing">
+        <h1 className="title">Hello World,</h1>
+        <p className="text">
+          Welcome to The Avery Rice Memorial Foundation.
+          As we construct our site, please check back regularly for updated content.
+          Through this site, we plan to run administration for our events and competitions. 
+          We also will post the latest information on our foundation and how you can 
+          join our Country Club. 
+          We will also have our merchandise for sale here and much, much more! 
+          For now visit our links in our link tree. 
+          More content coming soon.
+          </p>
+          <button><a className="linktree" href="https://linktr.ee/averyricememorialfoundation">Visit LinkTree</a></button>
+      </div>
     </div>
   );
 }
